c21f250f6c386fd13e6a8f3c2c437727666df235
Jason Rizio <jason.rizio@gmail.com>
2019-09-05 15:40:41 +1000

    upd2

--- scripts/startflood.ps1
+++ scripts/startflood.ps1
@@ -41,7 +41,7 @@ try {
     $outFloodID = $responseFull.uuid
     Write-Output "Flood ID is: $outFloodID"
     Write-Host "##vso[task.setvariable variable=FloodID;]$outFloodID"
-    Write-Host "Set environment variable to ($env:FLOODID)"
+    Write-Output "Flood ID environment variable is: $env:FLOODID"
 
 }
 catch {
